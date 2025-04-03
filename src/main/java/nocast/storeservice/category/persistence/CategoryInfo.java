8acf64c49061e59409ff64b28b886eb93286dc87
vnavesnoj <vnavesnoj@gmail.com>
2025-04-04 00:23:34 +0300

    Refactor CategoryInfo

--- src/main/java/nocast/storeservice/category/persistence/CategoryInfo.java
+++ src/main/java/nocast/storeservice/category/persistence/CategoryInfo.java
@@ -1,6 +1,7 @@
 package nocast.storeservice.category.persistence;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CategoryInfo {
 
     private String name;

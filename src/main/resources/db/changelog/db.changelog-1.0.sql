33dda01feef1e074a7c12ff1b090f01eb74b564c
vnavesnoj <vnavesnoj@gmail.com>
2025-04-03 19:45:26 +0300

    Update db.changelog-1.0.sql

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -1,4 +1,19 @@
 --liquibase formatted sql
 
 --changeset vnavesnoj:1
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE category
+(
+    id                  SERIAL PRIMARY KEY,
+    slug                VARCHAR(255) NOT NULL UNIQUE,
+    parent_id           INT          REFERENCES category (id) ON DELETE SET NULL,
+    sort_order          INT          NOT NULL DEFAULT 0,
+    level               INT          NOT NULL DEFAULT 0,
+    image               VARCHAR(255),
+    is_active           BOOLEAN               DEFAULT TRUE,
+    translations        JSONB        NOT NULL,
+    default_lang_code   VARCHAR(2)   NOT NULL,
+    name_default        VARCHAR(255) GENERATED ALWAYS AS (translations #>> ARRAY [default_lang_code, 'name']) STORED,
+    description_default TEXT GENERATED ALWAYS AS (translations #>> ARRAY [default_lang_code, 'description']) STORED,
+    created_at          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP
+);

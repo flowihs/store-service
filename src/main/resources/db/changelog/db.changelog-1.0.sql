ce8516bd4aebdd17ba93b2fcc00f1413db83534a
vnavesnoj <vnavesnoj@gmail.com>
2025-04-04 16:11:38 +0300

    Change db.changelog-1.0.sql

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -3,17 +3,15 @@
 --changeset vnavesnoj:1
 CREATE TABLE category
 (
-    id                  SERIAL PRIMARY KEY,
-    slug                VARCHAR(255) NOT NULL UNIQUE,
-    parent_id           INT          REFERENCES category (id) ON DELETE SET NULL,
-    sort_order          INT          NOT NULL DEFAULT 0,
-    level               INT          NOT NULL DEFAULT 0,
-    image               VARCHAR(255),
-    is_active           BOOLEAN               DEFAULT TRUE,
-    translations        JSONB        NOT NULL,
-    default_lang_code   VARCHAR(2)   NOT NULL,
-    name_default        VARCHAR(255) GENERATED ALWAYS AS (translations #>> ARRAY [default_lang_code, 'name']) STORED,
-    description_default TEXT GENERATED ALWAYS AS (translations #>> ARRAY [default_lang_code, 'description']) STORED,
-    created_at          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at          TIMESTAMP
+    id                SERIAL PRIMARY KEY,
+    slug              VARCHAR(255) NOT NULL UNIQUE,
+    parent_id         INT          REFERENCES category (id) ON DELETE SET NULL,
+    sort_order        INT          NOT NULL,
+    level             INT          NOT NULL,
+    image             VARCHAR(255),
+    is_active         BOOLEAN,
+    translations      JSONB        NOT NULL,
+    default_lang_code VARCHAR(7)   NOT NULL,
+    created_at        TIMESTAMP    NOT NULL,
+    updated_at        TIMESTAMP
 );

edbeff56890a877c647120d4fce02bd341202236
vnavesnoj <vnavesnoj@gmail.com>
2025-04-14 17:52:11 +0300

    Add CategoryTreeDto

--- src/main/java/nocast/storeservice/category/dto/CategoryTreeDto.java
+++ src/main/java/nocast/storeservice/category/dto/CategoryTreeDto.java
@@ -3,18 +3,20 @@ package nocast.storeservice.category.dto;
 import lombok.Value;
 import nocast.storeservice.category.persistence.CategoryInfo;
 
+import java.util.List;
+
 /**
  * @author vnavesnoj
  * @mail vnavesnoj@gmail.com
  */
 
 @Value
-public class CategoryReadDto {
+public class CategoryTreeDto {
     Integer id;
     Integer parentId;
-    String parentName;
     CategoryInfo info;
     Integer sortOrder;
     Integer level;
     String image;
+    List<CategoryTreeDto> subcategories;
 }

3136ddf9a4ca05ae902a4ae5d9fbe900f21a27d1
vnavesnoj <vnavesnoj@gmail.com>
2025-04-16 01:07:15 +0300

    Add simple CategoryServiceTest

--- src/test/java/nocast/storeservice/category/service/CategoryServiceTest.java
+++ src/test/java/nocast/storeservice/category/service/CategoryServiceTest.java
@@ -2,11 +2,18 @@ package nocast.storeservice.category.service;
 
 import lombok.RequiredArgsConstructor;
 import nocast.storeservice.TestcontainersConfiguration;
+import nocast.storeservice.category.dto.CategoryFilter;
+import nocast.storeservice.category.dto.TreeViewOptions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
 import org.springframework.modulith.test.ApplicationModuleTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_CLASS;
 
 /**
  * @author vnavesnoj
@@ -16,6 +23,7 @@ import org.springframework.test.annotation.Rollback;
 @RequiredArgsConstructor
 @Import(TestcontainersConfiguration.class)
 @Rollback
+@Sql(scripts = "classpath:test-category-data.sql", executionPhase = BEFORE_TEST_CLASS)
 @ApplicationModuleTest
 public class CategoryServiceTest {
 
@@ -23,9 +31,14 @@ public class CategoryServiceTest {
     private final CategoryService categoryService;
 
     @Test
-    void testService() {
-
+    void simpleTest() {
+        final var filter = CategoryFilter.builder().name("одежда").build();
+        final var page = categoryService.findAll(
+                new TreeViewOptions(0, 0), Pageable.unpaged(), filter
+        );
+        Assertions.assertThat(page)
+                .isNotEmpty()
+                .hasSize(2);
+        page.forEach(System.out::println);
     }
-
-
 }

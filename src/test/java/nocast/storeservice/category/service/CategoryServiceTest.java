b28bcf4a5f00f98e9307b52cda0bbcd4ac9981cf
Zaraza-it <egor76522@Inbox.ri>
2025-04-19 04:30:35 +0300

    added tests

--- src/test/java/nocast/storeservice/category/service/CategoryServiceTest.java
+++ src/test/java/nocast/storeservice/category/service/CategoryServiceTest.java
@@ -3,32 +3,42 @@ package nocast.storeservice.category.service;
 import lombok.RequiredArgsConstructor;
 import nocast.storeservice.TestcontainersConfiguration;
 import nocast.storeservice.category.dto.CategoryFilter;
+import nocast.storeservice.category.dto.CategoryReadDto;
 import nocast.storeservice.category.dto.TreeViewOptions;
+import nocast.storeservice.category.persistence.Category;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.modulith.test.ApplicationModuleTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_CLASS;
+import java.util.Locale;
+import java.util.Optional;
 
-/**
- * @author vnavesnoj
- * @mail vnavesnoj@gmail.com
- */
-
-@RequiredArgsConstructor
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @Import(TestcontainersConfiguration.class)
 @Rollback
-@Sql(scripts = "classpath:test-category-data.sql", executionPhase = BEFORE_TEST_CLASS)
 @ApplicationModuleTest
+@RequiredArgsConstructor
+@Sql(scripts = "/test-category-data.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class CategoryServiceTest {
-
     @Autowired
     private final CategoryService categoryService;
+
+    @BeforeEach
+    void setUp() {
+        LocaleContextHolder.setLocale(Locale.ENGLISH);
+    }
 
     @Test
     void simpleTest() {
@@ -41,4 +51,75 @@ public class CategoryServiceTest {
                 .hasSize(2);
         page.forEach(System.out::println);
     }
+
+
+    //Поиск всех категорий по дефолтной ветке/пейджу/фильтру
+    @Test
+    void findAllTest_defaultPage() {
+        Page<CategoryReadDto> page = categoryService.findAll();
+
+        Assertions.assertThat(page).isNotNull();
+        assertEquals(page.getTotalElements(), 3);
+        assertEquals(page.getContent().get(0).getInfo().getName(), "Electronics");
+    }
+
+    //Поиск всех категорий по глубине ветки/дерева || EN
+    @Test
+    void findAll_TreeViewOptions() {
+        final var tree = TreeViewOptions.builder().treeDepth(0).branchDepth(0).build();
+        Page<CategoryReadDto> page = categoryService.findAll(tree);
+
+        assertEquals(page.getContent().size(), 3);
+        assertEquals(page.getContent().get(0)
+                .getInfo().getName(), "Electronics");
+
+        assertEquals(page.getContent().get(1)
+                .getInfo().getName(), "Clothing");
+
+        assertEquals(page.getContent().get(2)
+                .getInfo().getName(), "Books");
+    }
+
+    //Поиск и фильтрация категории - Электроника/Electronics || EN || level = 0
+    @Test
+    void findAll_Filters() {
+        final var tree = TreeViewOptions.builder().treeDepth(0).branchDepth(0).build();
+        final var filter = CategoryFilter.builder().name("Electronics").level(0).isLeaf(false).isRoot(true).parentId(null).build();
+
+        Page<CategoryReadDto> page = categoryService.findAll(tree,Pageable.unpaged(Sort.by("sortOrder").ascending()),filter);
+
+        Assertions.assertThat(page).isNotNull();
+        assertEquals(page.getTotalElements(), 1);
+        assertEquals(page.getContent().size(), 1);
+        assertEquals(page.getContent().get(0).getInfo().getName(), filter.getName());
+    }
+
+    //Поиск категории - Электроника/Electronics || EN
+    @Test
+    void findById() {
+        Optional<CategoryReadDto> result = categoryService.findById(1);
+
+        Assertions.assertThat(result).isPresent();
+        assertEquals(result.get().getId(), 1);
+        assertEquals(result.get().getLevel(), 0);
+        assertEquals(result.get().getInfo().getName(), "Electronics");
+    }
+
+    //Поиск категории - Художественная/Fiction по фильтрам level1 && parent_id
+    @Test
+    void findAll_Filters_Level1() {
+        final var tree = TreeViewOptions.builder().treeDepth(0).branchDepth(0).build();
+        final var filter = CategoryFilter.builder().name("Fiction").level(1).isLeaf(false).isRoot(false).parentId(3).build();
+
+        Page<CategoryReadDto> page = categoryService.findAll(tree,Pageable.unpaged(Sort.by("sortOrder").ascending()),filter);
+
+        Assertions.assertThat(page).isNotNull();
+        assertEquals(page.getTotalElements(), 1);
+        assertEquals(page.getContent().size(), 1);
+        assertEquals(page.getContent().get(0)
+                .getInfo().getName(), filter.getName());
+        assertEquals(page.getContent().get(0)
+                .getInfo().getDescription(), "Novels and stories");
+    }
+
 }
